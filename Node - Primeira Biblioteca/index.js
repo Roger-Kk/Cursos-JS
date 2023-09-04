eaa622bb996002d4e12c446e7131336531181a40
Roger-Kk <rogerkolossoski@gmail.com>
2023-09-04 20:31:31 -0300

    Aula 3

--- Node - Primeira Biblioteca/index.js
+++ Node - Primeira Biblioteca/index.js
@@ -3,27 +3,55 @@ import fs from 'fs';
 
 console.log(chalk.blue('Olá mundo'));
 
-function trataErro(erro){
-    console.log(erro);
-    throw new Error (chalk.red(erro.code, 'Não há arquivo no diretório'));
-}
-//async/await
-/*async function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    console.log(texto);
-}*/
+//Texto guardado numa variável para testes antes de buscar no arquivo: 
+/*
+const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+*/
 
 async function pegaArquivo (caminhoDoArquivo){
     try{
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(chalk.green(texto))
+        console.log(extraiLinks(texto));
     } catch (erro){
         trataErro(erro)
     }
 }
 
+function extraiLinks(texto){
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)]; //método de manipulação de string
+    //const capturas = regex.exec(texto); //método de regex
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+    return resultados;
+}
+
+function trataErro(erro){
+    console.log(erro);
+    throw new Error (chalk.red(erro.code, 'Não há arquivo no diretório'));
+}
+
+
+pegaArquivo('./Arquivos/texto.md');
+
+//Para pegar o erro no código: 
+//pegaArquivo('./Arquivos/textossssss.md'); //erro
+
+
+
+// Regex para identificar tudo que está entre colchetes: 
+// \[[^[\]]*?\]
+// Regex para identificar o que está entre parênteses: 
+// \(https?:\/\/[^\s?#.].[^\s]*\)
+// Regex completo para selecionar o Título + URL: 
+// \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
+
+
+ //OBS: os (...) é um simbolo na linguagem que funciona como um "espalhador", visando retornar em uma string o conteúdo de um objeto iterável. Neste caso produzido pelo método string matchAll()
+
+
+
+/*-------------------------------------------- */
 
 // Promises con then()
 //function pegaArquivo(caminhoDoArquivo){
@@ -45,5 +73,4 @@ async function pegaArquivo (caminhoDoArquivo){
 //     })
 // }
 
-pegaArquivo('./Arquivos/texto.md');
-pegaArquivo('./Arquivos/textossssss.md'); //erro
+
