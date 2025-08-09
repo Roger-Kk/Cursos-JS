523327a488cf696d3f59fba0d03f63f2975fe27c
Roger-Kk <rogerkolossoski@gmail.com>
2025-08-09 12:03:33 -0300

    Aula 1

---
+++ Promisses, Async, Await/scripts.js
@@ -0,0 +1,8 @@
+
+//pegar elementos
+const uploadBtn = document.getElementById("upload-btn");
+const inputUpload = document.getElementById("image-upload");
+
+uploadBtn.addEventListener("click", () => {
+    inputUpload.click();
+})
